58b33db39037c30f0fb2f937e40f4525fdde658e
abovesky <abovesky@qq.com>
2020-01-04 14:56:31 +0800

    fixed Jwt facade

--- src/Facade/Jwt.php
+++ src/Facade/Jwt.php
@@ -8,6 +8,9 @@ class Jwt extends Facade
 {
     protected static function getFacadeClass()
     {
+        if (substr(app()->version(), 0 , 3) === '5.1') {
+            return \abovesky\Jwt::class;
+        }
         return 'jwt';
     }
 }
